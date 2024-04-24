46e1959d11d6a6468593ba6115ec9e29e41b8863
Christian <nielsenchristianole@gmail.com>
2024-04-24 13:57:27 +0200

    finished plane

--- src/image_clustering.py
+++ src/image_clustering.py
@@ -1,7 +1,10 @@
+from typing import Literal, Union
+
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
+from pylanczos import PyLanczos
 
 
 IMAGE_NAME = 'plane'
@@ -10,9 +13,9 @@ IMAGE_NAME = 'plane'
 params = dict(
     plane = dict(
         img_downscale = 6,
-        radius = 100,
+        radius = float('inf'),
         sigma_i = 0.5,
-        sigma_x = 145,
+        sigma_x = 8,
         k = 2,
         mode='gray'
     ),
@@ -41,76 +44,79 @@ params = dict(
 )
 
 
+class ImageSegmenter:
+
+    def __init__(
+        self,
+        *,
+        eigen_solver: Literal['full', 'lanczos'] = 'lanczos',
+        laplacian: Literal['unnorm', 'shi_malik', 'ng_jordan_weiss'] = 'shi_malik',
+        node_size: Literal['volume', 'degree'] = 'volume',
+    ) -> None:
+        self.eigen_solver = eigen_solver
+        self.laplacian = laplacian
+        self.node_size = node_size
+    
+    def __call__(self, similarity_matrix: np.ndarray, num_clusters: int, *, return_encodings: bool=False) -> Union[np.ndarray, tuple[np.ndarray, np.ndarray]]:
+        encodings = self.encode_nodes(similarity_matrix, num_clusters)
+        labels = self._cluster_encodings(encodings, num_clusters)
+        if return_encodings:
+            return labels, encodings
+        return labels
+
+    def _cluster_encodings(self, encodings: np.ndarray, num_clusters: int) -> np.ndarray:
+        kmeans = KMeans(n_clusters=num_clusters, random_state=0).fit(encodings)
+        return kmeans.labels_
+
+    def _get_node_size(self, similarity_matrix: np.ndarray) -> np.ndarray:
+        match self.node_size:
+            case 'volume':
+                return np.sum(similarity_matrix, axis=1)
+            case 'degree':
+                return np.count_nonzero(similarity_matrix, axis=1)
+    
+    def _get_eigen_decomposition(self, similarity_matrix: np.ndarray, num_clusters: int) -> np.ndarray:
+        match self.eigen_solver:
+            case 'full':
+                _, eig_vec =  np.linalg.eigh(similarity_matrix)
+            case 'lanczos':
+                solver = PyLanczos(similarity_matrix, find_maximum=False, num_eigs=num_clusters+1)
+                _, eig_vec = solver.run()
+        return eig_vec[:, 1:1+num_clusters]
+
+    def encode_nodes(self, similarity_matrix: np.ndarray, num_clusters: int) -> np.ndarray:
+        d = self._get_node_size(similarity_matrix)
+        D = np.diag(d)
+        D_sqrt_inv = np.diag(1 / np.sqrt(d))
+
+        match self.laplacian:
+            case 'unnorm':
+                L = D - similarity_matrix
+                return self._get_eigen_decomposition(L, num_clusters)
+            case 'shi_malik':
+                L = D - similarity_matrix
+                L_sym = D_sqrt_inv @ L @ D_sqrt_inv
+                eig_vec = self._get_eigen_decomposition(L_sym, num_clusters)
+                return D_sqrt_inv @ eig_vec
+            case 'ng_jordan_weiss':
+                L = D - similarity_matrix
+                L_sym = D_sqrt_inv @ L @ D_sqrt_inv
+                eig_vec = self._get_eigen_decomposition(L_sym, num_clusters)
+                return eig_vec / np.linalg.norm(eig_vec, axis=1, keepdims=True)
+
+cluster = ImageSegmenter(
+    eigen_solver = 'full',
+    laplacian = 'shi_malik',
+    node_size = 'volume'
+)
+
+
 img_downscale = params[IMAGE_NAME]['img_downscale']
 radius = params[IMAGE_NAME]['radius']
 sigma_i = params[IMAGE_NAME]['sigma_i']
 sigma_x = params[IMAGE_NAME]['sigma_x']
 k = params[IMAGE_NAME]['k']
 mode = params[IMAGE_NAME]['mode']
-
-
-def unnormalized_spectral_clustering(similarity_matrix: np.ndarray, k: int) -> np.ndarray:
-
-    volume = np.sum(similarity_matrix, axis=1)
-
-    D = np.diag(volume)
-    L = D - similarity_matrix
-
-    eig_val, eig_vec = np.linalg.eigh(L)
-
-    U = eig_vec[:, 1:1+k]
-
-    kmeans = KMeans(n_clusters=k, random_state=0).fit(U)
-    return kmeans.labels_
-
-
-def normalized_spectral_clustering(similarity_matrix: np.ndarray, k: int) -> np.ndarray:
-
-    volume = np.sum(similarity_matrix, axis=1)
-
-    D = np.diag(volume)
-    L = D - similarity_matrix
-
-    D_sqrt = np.diag(np.sqrt(volume))
-    D_sqrt_inv = np.diag(1 / np.sqrt(volume))
-    # D_inv = np.diag(1 / d_i)
-
-    L_sym = D_sqrt_inv @ L @ D_sqrt_inv
-    # L_rw = D_inv @ L
-
-    eig_val, eig_vec = np.linalg.eigh(L_sym)
-
-    # rw_eig_vec = D_sqrt @ eig_vec
-    rw_eig_vec = D_sqrt_inv @ eig_vec
-
-    U = rw_eig_vec[:, 1:1+k]
-
-    kmeans = KMeans(n_clusters=k, random_state=0).fit(U)
-    return kmeans.labels_
-
-
-def normalized_spectral_clustering_advanced(similarity_matrix: np.ndarray, k: int) -> np.ndarray:
-
-    volume = np.sum(similarity_matrix, axis=1)
-
-    D = np.diag(volume)
-    L = D - similarity_matrix
-
-    D_sqrt = np.diag(np.sqrt(volume))
-    D_sqrt_inv = np.diag(1 / np.sqrt(volume))
-    # D_inv = np.diag(1 / d_i)
-
-    L_sym = D_sqrt_inv @ L @ D_sqrt_inv
-    # L_rw = D_inv @ L
-
-    eig_val, eig_vec = np.linalg.eigh(L_sym)
-    T = eig_vec / np.linalg.norm(eig_vec, axis=1, keepdims=True)
-
-    U = T[:, 1:1+k]
-
-    kmeans = KMeans(n_clusters=k, random_state=0).fit(U)
-    return kmeans.labels_
-
 
 
 # load image
@@ -157,23 +163,24 @@ mask = dist <= radius
 int_diffs = np.linalg.norm(img[row_inx] - img[col_idx], axis=1)
 
 # calculate the intensity, spatial and joint weights
-w_i = (np.exp(-int_diffs / sigma_i)).reshape(mat_lenth, mat_lenth)
-w_x = (np.exp(-dist / sigma_x) * mask).reshape(mat_lenth, mat_lenth)
+w_i = (np.exp(-int_diffs / sigma_i**2)).reshape(mat_lenth, mat_lenth)
+w_x = (np.exp(-dist / sigma_x**2) * mask).reshape(mat_lenth, mat_lenth)
 
-w_ij = np.exp((-int_diffs / sigma_i) + (-dist / sigma_x)) * mask
+w_ij = np.exp((-int_diffs / sigma_i**2) + (-dist / sigma_x**2)) * mask
 w_ij = w_ij.reshape(mat_lenth, mat_lenth)
 
 # visualize the weights matrices
-# fig, axs = plt.subplots(1, 3, figsize=(15, 5))
-# axs[0].imshow(w_i, cmap='gray')
-# axs[1].imshow(w_x, cmap='gray')
-# axs[2].imshow(w_ij, cmap='gray')
-# axs[0].set_title('Intensity')
-# axs[1].set_title('Spatial')
-# axs[2].set_title('Joint')
-# for ax in axs:
-#     ax.axis('off')
-# fig.tight_layout()
+fig, axs = plt.subplots(1, 3, figsize=(15, 5))
+axs[0].imshow(w_i, cmap='gray')
+axs[1].imshow(w_x, cmap='gray')
+axs[2].imshow(w_ij, cmap='gray')
+axs[0].set_title('Intensity')
+axs[1].set_title('Spatial')
+axs[2].set_title('Joint')
+for ax in axs:
+    ax.axis('off')
+fig.tight_layout()
+fig.savefig(f'results/{IMAGE_NAME}_affinity_matrix.pdf', bbox_inches='tight', pad_inches=0)
 # plt.show()
 
 colors = np.array([
@@ -187,15 +194,11 @@ colors = np.array([
 
 
 fig, axs = plt.subplots(3, 3, figsize=(15, 15))
-# fig, axs = plt.subplots(1, 3, figsize=(15, 5))
 axs = axs if len(axs.shape) == 2 else np.array([axs])
 
 metrices = [w_ij, w_i, w_x]
-metrices = 3*[w_ij]
-clustering_methods = [unnormalized_spectral_clustering, normalized_spectral_clustering, normalized_spectral_clustering_advanced]
-
-for row, (_cluster_fn, similarity_matrix, _axs) in enumerate(zip(clustering_methods, metrices, axs)):
-    labels = _cluster_fn(similarity_matrix, k)
+for row, (similarity_matrix, _axs) in enumerate(zip(metrices, axs)):
+    labels, _encodings = cluster(similarity_matrix, k, return_encodings=True)
     segmented_img = labels.reshape(before_shape)
     colored_segmentations = colors[segmented_img]
     colored_image = colored_segmentations * img_gray[:, :, None]
@@ -215,6 +218,7 @@ for row, (_cluster_fn, similarity_matrix, _axs) in enumerate(zip(clustering_meth
         _axs[0].set_xlabel('Original')
         _axs[1].set_xlabel('Colored by cluster')
         _axs[2].set_xlabel('Segmentations')
+        encodings = _encodings
     
     _axs[0].set_ylabel(('Joint', 'Intensity', 'Spatial')[row])
 
@@ -227,4 +231,14 @@ for ax in axs.flatten():
 
 fig.tight_layout()
 fig.savefig(f'results/{IMAGE_NAME}_segmented.pdf', bbox_inches='tight', pad_inches=0)
+# plt.show()
+
+ordinal_indicator = {1: 'st', 2: 'nd', 3: 'rd'}
+fig, axs = plt.subplots(1, k, figsize=(5*k, 5))
+for idx, (ax, encoding) in enumerate(zip(axs.flatten(), encodings.T), start=2):
+    ax.imshow(encoding.reshape(before_shape), cmap='gray')
+    ax.axis('off')
+    ax.set_title(f"{idx}'{ordinal_indicator.get(idx, 'th')} eigenvector")
+fig.tight_layout()
+fig.savefig(f'results/{IMAGE_NAME}_eig_vecs.pdf', bbox_inches='tight', pad_inches=0)
 plt.show()
